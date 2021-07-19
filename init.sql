10e6b3a0b424150750e4a87741f1985e59dc15e1
shasuri <ghimmk@gmail.com>
2021-07-19 21:54:47 +0900

    table init updated

--- init.sql
+++ init.sql
@@ -14,43 +14,58 @@ USE summer;
 
 CREATE TABLE user{
     global_user_no INT PRIMARY KEY AUTO_INCREMENT,
+    user_auth_token NVARCHAR(40),
+    user_name NVARCHAR(20) NOT NULL,
     user_id NVARCHAR(20) NOT NULL,
-    user_pw NVARCHAR(20) NOT NULL,
-    user_name NVARCHAR(20) NOTNULL,
-    user_email NVARCHAR(40),
-    user_gender CHAR(1) NOT NULL,
-    user_birthday_year INT,
-    user_birthday_month INT,
-    user_birthday_day INT,
     user_join_dt DATETIME NOT NULL
-    /*joined room list?*/
-}
-
-CREATE TABLE skin{
-    skin_user_no INT,
-    FOREIGN KEY (skin_user_no) REFERENCES user (global_user_no),
-    skin_color INT NOT NULL,
-    skin_role CHAR(1) NOT NULL, /*student or teacher*/
-
+    /*
+        user_email NVARCHAR(40),
+        user_gender CHAR(1) NOT NULL,
+        user_birthday_year INT,
+        user_birthday_month INT,
+        user_birthday_day INT,
+    */
 }
 
 CREATE TABLE room{
     global_room_no INT PRIMARY KEY AUTO_INCREMENT,
     room_name NVARCHAR(20) NOT NULL,
+    room_code CHAR(5) NOT NULL,
     room_present INT NOT NULL,
     room_max INT NOT NULL,
     room_create_dt DATETIME NOT NULL
-    /*room member list?*/
 }
 
+CREATE TABLE joining{ /*match user and room*/
+    joining_user_no INT,
+    FOREIGN KEY (joining_user_no) REFERENCES user (global_user_no),
+    joining_room_no INT,
+    FOREIGN KEY (joining_room_no) REFERENCES room (global_room_no),
+}
 
+CREATE TABLE skin{
+    skin_color INT NOT NULL,
+    skin_role CHAR(1) NOT NULL, /*student or teacher*/
+    skin_user_no INT,
+    FOREIGN KEY (skin_user_no) REFERENCES user (global_user_no),
+}
+
+CREATE TABLE timer{
+    timer_subject NVARCHAR(20) NOT NULL,
+    timer_start_time DATETIME NOT NULL,
+    timer_acc_time INT NOT NULL,
+    timer_user_no INT,
+    FOREIGN KEY (timer_user_no) REFERENCES user (global_user_no),
+    timer_room_no INT,
+    FOREIGN KEY (timer_room_no) REFERENCES room (global_room_no),
+}
 
 /*
     로그 테이블 / 단일? 복수?
 */
 CREATE TABLE logs{
-    login_time DATETIME,
-    login_status VARCHAR(45),
-    log_id INT,
-    FOREIGN KEY (log_id) REFERENCES management (id)
+    logs_time DATETIME,
+    logs_status VARCHAR(45),
+    logs_id INT,
+    FOREIGN KEY (logs_id) REFERENCES user (global_user_no)
 };
