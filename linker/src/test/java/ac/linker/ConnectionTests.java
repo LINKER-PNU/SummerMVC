d822e4ba59984a69e3e6ea5dfc63efa66d7f23a6
shasuri <ghimmk@gmail.com>
2021-07-22 22:22:12 +0900

    disable test

--- linker/src/test/java/ac/linker/ConnectionTests.java
+++ linker/src/test/java/ac/linker/ConnectionTests.java
@@ -1,23 +1,23 @@
-package ac.linker;
+// package ac.linker;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-import ac.linker.service.ConnectService;
+// import ac.linker.service.ConnectService;
 
-@SpringBootTest
-public class ConnectionTests {
-    private ConnectService connectService;
+// @SpringBootTest
+// public class ConnectionTests {
+// private ConnectService connectService;
 
-    @Autowired
-    ConnectionTests(ConnectService connectService) {
-        this.connectService = connectService;
-    }
+// @Autowired
+// ConnectionTests(ConnectService connectService) {
+// this.connectService = connectService;
+// }
 
-    @Test
-    public void test() {
-        System.out.println("############test############");
-        System.out.println(connectService.getUser());
-    }
-}
+// @Test
+// public void test() {
+// System.out.println("############test############");
+// System.out.println(connectService.getUser());
+// }
+// }
