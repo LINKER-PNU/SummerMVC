6a4fe736f1d85e9a4dfc6cb94cfc8410c5190159
shasuri <ghimmk@gmail.com>
2021-07-28 22:35:38 +0900

    disable base64

--- linker/src/main/java/ac/linker/service/CodeGenerator.java
+++ linker/src/main/java/ac/linker/service/CodeGenerator.java
@@ -1,23 +1,23 @@
-package ac.linker.service;
+// package ac.linker.service;
 
-import org.springframework.stereotype.Service;
+// import org.springframework.stereotype.Service;
 
-@Service
-public class CodeGenerator {
-    static final String base = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+// @Service
+// public class CodeGenerator {
+//     static final String base = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-    private String base62Encode(int text) {
-        final StringBuilder sb = new StringBuilder();
-        do {
-            int i = text % 62;
-            sb.append(base.charAt(i));
-            text /= 62;
-        } while (text > 0);
+//     private String base62Encode(int text) {
+//         final StringBuilder sb = new StringBuilder();
+//         do {
+//             int i = text % 62;
+//             sb.append(base.charAt(i));
+//             text /= 62;
+//         } while (text > 0);
 
-        return sb.toString();
-    }
+//         return sb.toString();
+//     }
     
-    public static String getCode(void){
+//     public static String getCode(void){
         
-    } 
-}
+//     } 
+// }
