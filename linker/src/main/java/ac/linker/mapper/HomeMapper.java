2c1cc5b79b900e238c79390b7f39f4c63954f99a
shasuri <ghimmk@gmail.com>
2021-08-08 01:06:42 +0900

    Seperate home services

---
+++ linker/src/main/java/ac/linker/mapper/HomeMapper.java
@@ -0,0 +1,32 @@
+package ac.linker.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import ac.linker.dto.UserDto;
+
+@Repository
+@Mapper
+public interface HomeMapper {
+
+    void insertUser(UserDto user);
+
+    void updateToken(UserDto user);
+
+    void updateSkin(UserDto user);
+
+    List<Map<String, Object>> getAllUser();
+
+    List<Map<String, Object>> getUser(UserDto user);
+
+    List<Map<String, Object>> getSkin(UserDto user);
+
+    List<Map<String, Object>> getRoom(UserDto user);
+
+    List<Map<String, Object>> getUserByName(UserDto user);
+
+    List<Map<String, Object>> getRoomByName(UserDto user);
+}
