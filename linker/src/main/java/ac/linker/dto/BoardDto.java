d3e3df7591b4f77ad0b5fea9ca67a74cc7435aef
shasuri <ghimmk@gmail.com>
2021-08-12 15:46:19 +0900

    Remove useless dto methods

--- linker/src/main/java/ac/linker/dto/BoardDto.java
+++ linker/src/main/java/ac/linker/dto/BoardDto.java
@@ -12,36 +12,11 @@ public class BoardDto {
     private String boardTitle;
     private String boardContent;
 
-    private String boardWriteDt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());;
-    private String boardEditDt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());;
+    private String boardWriteDt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+    private String boardEditDt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
     private String boardDeadline;
 
     private boolean boardNotice;
     private boolean boardAssignment;
     private boolean boardVisible = true;
-
-    public int getBoardId() {
-        return boardId;
-    }
-
-    public void setBoardId(int boardId) {
-        this.boardId = boardId;
-    }
-
-    public String getBoardTitle() {
-        return boardTitle;
-    }
-
-    public void setBoardTitle(String boardTitle) {
-        this.boardTitle = boardTitle;
-    }
-
-    public String getBoardRoom() {
-        return boardRoom;
-    }
-
-    public void setBoardRoom(String boardRoom) {
-        this.boardRoom = boardRoom;
-    }
-
 }
