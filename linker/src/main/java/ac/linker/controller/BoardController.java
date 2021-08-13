e2db42b0eb09870153cc47df61f74b2206b3f86d
JaewookLee <ghimmk@gmail.com>
2021-08-13 11:36:42 +0900

    Refactor board controller logs and edit board query.

--- linker/src/main/java/ac/linker/controller/BoardController.java
+++ linker/src/main/java/ac/linker/controller/BoardController.java
@@ -3,6 +3,8 @@ package ac.linker.controller;
 import com.google.gson.Gson;
 
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +19,8 @@ import ac.linker.vo.BoardVo;
 @RestController
 @RequestMapping(value = "/board")
 public class BoardController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     private BoardService boardService;
     private ResponseService responseService;
 
@@ -32,46 +36,56 @@ public class BoardController {
     // get board list
     @PostMapping(value = "/list", produces = "application/json; charset=utf8")
     public String getBoards(@RequestBody BoardVo boardVo) {
+        logger.info("getBoards :: " + boardVo.getBoardRoom());
         BoardDto boardDto = modelMapper.map(boardVo, BoardDto.class);
+
+        logger.info("Boards in room " + boardVo.getBoardRoom() + " select complete.\n" );
         return gson.toJson(boardService.getBoards(boardDto));
     }
 
     // click board
     @PostMapping(value = "/content", produces = "application/json; charset=utf8")
     public String getBoardContent(@RequestBody BoardVo boardVo) {
+        logger.info("getBoardContent :: " + boardVo.getBoardId());
         BoardDto boardDto = modelMapper.map(boardVo, BoardDto.class);
+
+        logger.info("Content of (ID)" + boardVo.getBoardId() + " select complete.\n");
         return boardService.getBoardContent(boardDto).getBoardContent();
     }
 
     // write board
     @PostMapping(value = "/insert", produces = "application/json; charset=utf8")
     public String insertBoard(@RequestBody BoardVo boardVo) {
-
+        logger.info("insertBoard :: " + boardVo.getBoardRoom() + " :: " + boardVo.getBoardTitle() + " :: " + boardVo.getBoardWriterId());
         BoardDto boardDto = modelMapper.map(boardVo, BoardDto.class);
 
         boardService.insertBoard(boardDto);
 
+        logger.info(boardVo.getBoardTitle() + " insert complete.\n");
         return responseService.getResultResponse(true);
     }
 
     // edit board
     @PostMapping(value = "/edit", produces = "application/json; charset=utf8")
     public String editBoard(@RequestBody BoardVo boardVo) {
+        logger.info("editBoard :: " + boardVo.getBoardId());
 
         BoardDto boardDto = modelMapper.map(boardVo, BoardDto.class);
         boardService.editBoard(boardDto);
 
+        logger.info("Board " + boardVo.getBoardId() + " :: " + boardVo.getBoardTitle() + " update complete.\n" );
         return responseService.getResultResponse(true);
     }
 
     // delete(deactivate) board
     @PostMapping(value = "/delete", produces = "application/json; charset=utf8")
     public String deleteBoard(@RequestBody BoardVo boardVo) {
-
+        logger.info("deleteBoard :: " + boardVo.getBoardId());
         BoardDto boardDto = modelMapper.map(boardVo, BoardDto.class);
 
         boardService.invisibleBoard(boardDto);
 
+        logger.info("Board " + boardVo.getBoardId() + " deactivate complete.\n" );
         return responseService.getResultResponse(true);
     }
 }
