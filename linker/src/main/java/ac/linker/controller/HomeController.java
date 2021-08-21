c7ccf2144cb801ce744794aa5c2b0c207404bfc3
shasuri <ghimmk@gmail.com>
2021-08-21 19:31:43 +0900

    Modify user cloth query and responser

--- linker/src/main/java/ac/linker/controller/HomeController.java
+++ linker/src/main/java/ac/linker/controller/HomeController.java
@@ -94,6 +94,7 @@ public class HomeController {
             userInfo.put("user_name", userResult.get(0).get("user_name"));
             userInfo.put("user_skin_color", userResult.get(0).get("user_skin_color"));
             userInfo.put("user_skin_role", userResult.get(0).get("user_skin_role"));
+            userInfo.put("user_skin_cloth", userResult.get(0).get("user_skin_cloth"));
         } else {
             userInfo.put("result_user", "empty set");
         }
