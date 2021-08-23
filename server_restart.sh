5654f66e22925154fbc73eceb716bb99863152c2
JaewookLee <ghimmk@gmail.com>
2021-08-23 10:29:31 +0900

    Modify server script to kill process after building.

--- server_restart.sh
+++ server_restart.sh
@@ -1,8 +1,8 @@
 #/bin/bash
-PROC=`ps -ef | grep "java -jar" | awk '{print $2}' |  head -n 1`
-kill -9 $PROC
 cd ~/SummerMVC/linker
 chmod +x ./gradlew
 ./gradlew build -x test
+PROC=`ps -ef | grep "java -jar" | awk '{print $2}' |  head -n 1`
+kill -9 $PROC
 nohup java -jar -Duser.timezone=KST build/libs/linker-0.0.1-SNAPSHOT.jar &
 tail -f ~/SummerMVC/linker/nohup.out
